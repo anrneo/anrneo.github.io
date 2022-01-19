83e48eaac6c951b05b3c0c7baf0ada4feeca4342
anreno <anreno@gmail.com>
2022-01-19 11:02:06 -0500

    NSSN-4790 Please replace this with New ServiceHub Crm logo

--- src/components/IntroContainer/IntroContainer.tsx
+++ src/components/IntroContainer/IntroContainer.tsx
@@ -67,7 +67,6 @@ const useStyles = makeStyles((theme: Theme) => ({
   twilioLogo: {
     position: 'relative',
     top: -30,
-    left: -20,
     [theme.breakpoints.down('sm')]: {
       display: 'none',
     },
