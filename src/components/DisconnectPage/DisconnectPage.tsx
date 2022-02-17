5c91d866495fe8b87be06f1f50dbfd459d947b35
anreno <anreno@gmail.com>
2022-02-16 21:52:28 -0500

    NSSN-4925 Customer shouldnt be able to rejoin the room, when service login users disconnects it

--- src/components/DisconnectPage/DisconnectPage.tsx
+++ src/components/DisconnectPage/DisconnectPage.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { makeStyles, Theme, CardContent, CardMedia, Grid, Card, Button } from '@material-ui/core';
+import { makeStyles, Theme, CardMedia, Grid, Card, Button } from '@material-ui/core';
 
 const useStyles = makeStyles((theme: Theme) => ({
   button: {
@@ -10,32 +10,43 @@ const useStyles = makeStyles((theme: Theme) => ({
     boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
     color: 'white',
     height: 100,
-    padding: '0 30px',
+    padding: '50px 30px',
   },
   CardContent: {
     justifyContent: 'center',
     alignItems: 'center',
-
     paddingTop: '150px',
+  },
+  CardMedia: {
+    [theme.breakpoints.down('xs')]: {
+      maxWidth: 350,
+      maxHeight: 200,
+      display: 'block',
+      margin: 'auto',
+    },
   },
 }));
 
 export default function DisconnectPage() {
   const classes = useStyles();
+  const logo = window.location.search.split('=')[1];
 
   return (
-    <Grid item className={classes.CardContent}>
-      <Card>
-        <CardMedia
-          component="img"
-          alt="green iguana"
-          height="330"
-          image="https://storage.servicehubcrm.net/dev/user_profile/userprofile_5577_SltTvGrWXy.jpeg"
-        />
-        <CardContent>
-          <Button className={classes.button}>Call has been disconnected</Button>
-        </CardContent>
-      </Card>
+    <Grid container className={classes.CardContent}>
+      <Grid item xs="auto">
+        <Card>
+          <CardMedia
+            className={classes.CardMedia}
+            component="img"
+            alt="green iguana"
+            height="330"
+            image={`https://storage.servicehubcrm.net/dev/user_profile/${logo}`}
+          />
+        </Card>
+      </Grid>
+      <Grid container item xs={12} className={classes.CardContent}>
+        <Button className={classes.button}>Call has been disconnected</Button>
+      </Grid>
     </Grid>
   );
 }
